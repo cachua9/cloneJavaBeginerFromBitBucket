248ed0e39390d3db4805046fae1fc3eff62d60e9
Hoàng Thành Lâm <meolam1999@gmail.com>
2020-03-03 18:23:22 +0000

    ChoosingOption.java edited online with Bitbucket for test

--- Lab02/JavaBasics/src/ChoosingOption.java
+++ Lab02/JavaBasics/src/ChoosingOption.java
@@ -4,5 +4,6 @@ public class ChoosingOption {
 		int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?");
 		JOptionPane.showMessageDialog(null, "You've chosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
 		System.exit(0);
+		//test
 	}
 }
