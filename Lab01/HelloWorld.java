fbbe057ed34cb80c393d6984595fa6d5462f1d79
Hoàng Thành Lâm <meolam1999@gmail.com>
2020-04-10 16:57:17 +0700

    All change

--- Lab01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-// Example 1: HelloWorld.java
-public class HelloWorld {
-    public static void main(final String[] args) {
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    }
-}
